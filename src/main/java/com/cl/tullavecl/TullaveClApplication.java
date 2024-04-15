d5549545fd26d2f14496ef53859b96f595394481
Luis Garcia <luis.garcia@mqaamericas.com>
2024-04-15 12:31:51 -0400

    Primer commit

---
+++ src/main/java/com/cl/tullavecl/TullaveClApplication.java
@@ -0,0 +1,13 @@
+package com.cl.tullavecl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TullaveClApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TullaveClApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cl/tullavecl/TullaveClApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cl.tullavecl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TullaveClApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
