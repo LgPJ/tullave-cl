31ee08218ecd7d2381c29254449114d52ef2ea5b
Luis Garcia <luis.garcia@mqaamericas.com>
2024-04-16 09:53:07 -0400

    fix: cors

--- src/main/java/com/cl/tullavecl/controllers/CardController.java
+++ src/main/java/com/cl/tullavecl/controllers/CardController.java
@@ -5,6 +5,7 @@ import com.cl.tullavecl.services.CardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 public class CardController {
 
